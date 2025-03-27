4736447068fc686467152d1dfa27d4a7f82b7d05
Masoud Kaviani <kaviani.masoud@gmail.com>
2025-03-27 15:50:18 +0330

    dvc repro

--- src/split.py
+++ src/split.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('data/cleaned_dataset.csv')
 X = df[['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz']]
 y = df['Gesture']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
 X_train.to_csv('temp_data/X_train.csv', index_label=False, index=False)
 X_test.to_csv('temp_data/X_test.csv', index_label=False, index=False)
